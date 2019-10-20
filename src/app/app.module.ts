a3e42cd1452c2b4aa1b5484664ae47c5d06f9802
Mustafa Abdulkareem <mabdulkareem@epitec.com>
2019-10-20 16:43:38 -0400

    add dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './features/shared/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './features/shared/shared.module';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
